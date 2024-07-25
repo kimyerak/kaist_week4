2def8585e83cb3fca4df7143d116d1660378e951
kimyerak <yerak213@gmail.com>
2024-07-25 20:06:57 +0900

    mogoDB URL 수정3

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,8 +14,9 @@ import { MissionsModule } from './missions/mission.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb://yerak213:yerak0213@localhost:27017/couple',
+      'mongodb://yerak213:yerak0213@localhost:27017/couple?authSource=admin&useNewUrlParser=true&useUnifiedTopology=true',
     ),
+
     MongooseModule.forFeature([{ name: Couple.name, schema: CoupleSchema }]),
     UsersModule,
     CalendarModule,
